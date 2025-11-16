1e5c7c4542aa5bb5581601bf38841c199c6d1ad7
faithiheabata <faithiheabata1@gmail.com>
2025-11-16 15:18:39 +0100

    modified file of 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -9,12 +9,5 @@ if (args.length === 0) {
 } else {
   console.log("Arguments found");
 }
-#!/usr/bin/node
 
-const args = process.argv[2];
 
-if (args === undefined) {
-  console.log("No argument");
-} else {
-  console.log(args);
-}
